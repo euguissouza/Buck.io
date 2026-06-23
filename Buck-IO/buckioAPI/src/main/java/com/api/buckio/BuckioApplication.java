d4afe539f245ccb51ef06919b8b87a9f5cd86fea
euguissouza <contato.guisilva4@hotmail.com>
2026-06-23 09:00:41 -0300

    config-inicial

---
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/BuckioApplication.java
@@ -0,0 +1,13 @@
+package com.api.buckio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckioApplication.class, args);
+	}
+
+}
---
+++ Buck-IO/buckioAPI/src/test/java/com/api/buckio/BuckioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.buckio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
