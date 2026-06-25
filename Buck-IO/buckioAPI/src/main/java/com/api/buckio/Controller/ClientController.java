8b2f66d7ef29d6291e62e8a5bdb9137934aa3280
euguissouza <contato.guisilva4@hotmail.com>
2026-06-25 04:48:22 -0300

    Controllers-Created

---
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/Controller/ClientController.java
@@ -0,0 +1,48 @@
+package com.api.buckio.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.buckio.DTO.ClientDTO;
+import com.api.buckio.Entities.Client;
+import com.api.buckio.Repository.ClientRepository;
+import com.api.buckio.Service.ClientService;
+
+@RestController
+@RequestMapping("/api/buck.io/Clients")
+public class ClientController {
+
+    private ClientService service;
+    private ClientRepository repository;
+
+    public ClientController(ClientService service, ClientRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    
+
+    @PostMapping("/post")
+    public Client newClient(ClientDTO dto){
+        Client client = service.CreateClient(dto);
+        return client;
+    }
+
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Client>>findClient(){
+        List<Client> find = repository.findAll();
+
+        return ResponseEntity.ok(find);
+    }
+
+    
+
+
+
+}
---
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/Controller/CollectionController.java
@@ -0,0 +1,41 @@
+package com.api.buckio.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.buckio.DTO.CollectionDTO;
+import com.api.buckio.Entities.Collection;
+import com.api.buckio.Repository.CollectionRepository;
+import com.api.buckio.Service.CollectionService;
+
+@RestController
+@RequestMapping("/api/buck.io/Collection")
+public class CollectionController {
+    
+    private CollectionService service;
+    private CollectionRepository repository;
+
+    public CollectionController(CollectionService service, CollectionRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    @PostMapping("/post")
+    public Collection newCollection(CollectionDTO dto){
+        Collection collection = service.CreateCollection(dto);
+        return collection;
+    }
+
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Collection>> findCollection(){
+        List<Collection> find = repository.findAll();
+        return ResponseEntity.ok(find);
+    }
+
+}
