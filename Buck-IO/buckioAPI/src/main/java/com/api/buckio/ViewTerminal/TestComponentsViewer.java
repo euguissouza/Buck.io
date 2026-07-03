5eb360cfe20453a1469409cea50da94d2ce707a2
euguissouza <contato.guisilva4@hotmail.com>
2026-07-02 21:25:11 -0300

    viewer-terminal

---
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/ViewTerminal/TestComponentsViewer.java
@@ -0,0 +1,38 @@
+package com.api.buckio.ViewTerminal;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.api.buckio.DTO.ClientDTO;
+import com.api.buckio.Entities.Client;
+import com.api.buckio.Repository.ClientRepository;
+import com.api.buckio.Service.RequestEngine;
+
+public class TestComponentsViewer {
+    Scanner scanner = new Scanner(System.in);
+
+    private ClientRepository repository;    
+    private RequestEngine engine;
+
+    public TestComponentsViewer(ClientRepository repository, RequestEngine engine) {
+        this.repository = repository;
+        this.engine = engine;
+    }
+
+
+
+
+    public void Viewer(ClientDTO dto){
+        List<Client> clients = repository.findAll();
+
+        System.out.println("Selecione o Client: ");
+        Long setInput = scanner.nextLong();
+        
+        if(setInput.equals(clients)){
+            engine.PostRequest(dto);
+        }
+
+
+    }
+ 
+}
