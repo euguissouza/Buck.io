c42293815624343d6de80c5dbc42b9802154d320
euguissouza <contato.guisilva4@hotmail.com>
2026-07-15 17:42:59 -0300

    Feat - WebClientConfiguration

---
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/Configuration/WebClientConfiguration.java
@@ -0,0 +1,34 @@
+package com.api.buckio.Configuration;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.api.buckio.Entities.Client;
+
+@Configuration
+public class WebClientConfiguration {
+
+    private Client client;
+    
+
+    public WebClientConfiguration(Client client) {
+        this.client = client;
+    }
+
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder){
+        return builder.
+        baseUrl(client.getUrl())
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+        .build();
+        
+    }
+
+}
