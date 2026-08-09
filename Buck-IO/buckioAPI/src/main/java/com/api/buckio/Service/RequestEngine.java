252a6dfe7ff6cf58482c08a117058ff1263120b8
euguissouza <contato.guisilva4@hotmail.com>
2026-08-09 02:37:47 -0300

    QUEBRADO QUEBRADO QUEBRADO

--- Buck-IO/buckioAPI/src/main/java/com/api/buckio/Service/RequestEngine.java
+++ Buck-IO/buckioAPI/src/main/java/com/api/buckio/Service/RequestEngine.java
@@ -6,6 +6,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.api.buckio.DTO.ClientDTO;
 import com.api.buckio.Entities.Client;
+import com.api.buckio.Repository.ClientRepository;
 
 import reactor.core.publisher.Mono;
 
@@ -13,17 +14,21 @@ import reactor.core.publisher.Mono;
 public class RequestEngine {
   
     private  WebClient webClient;
+    private ClientRepository repositoryClient;
 
 
-    public RequestEngine(WebClient webClient) {
+    public RequestEngine(WebClient webClient, ClientRepository repositoryClient) {
         this.webClient = webClient;
+        this.repositoryClient = repositoryClient;
 
     }
 
     
     public Mono<ClientDTO>RequestGet(Client client){
+        Client clientBanco = repositoryClient.findById(client.getId())
+        .orElseThrow(() -> new RuntimeException("Url vazia"));
         return webClient.get()
-                .uri(client.getUrl())
+                .uri(clientBanco.getUrl())
                 .retrieve()
                 .bodyToMono(ClientDTO.class);
     }
